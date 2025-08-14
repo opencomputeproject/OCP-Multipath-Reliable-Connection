b8a54fef7f040c12fcf7326ca8e68f83fa0e41bd
Rip Sohan <rips@amd.com>
2025-08-14 15:19:12 -0700

    Ctl: Add gid_tbl_len to mrc_attr structure and create cleanup file
    Signed-off-by: Rip Sohan <rips@amd.com>

--- mrc.h
+++ mrc.h
@@ -65,6 +65,9 @@ struct mrc_attr {
 	 */
 	uint32_t mrc_version;
 
+	/* Source GID table length.*/
+	int gid_tbl_len;
+
 	struct {
 		/* Max configurable wimm value as requestor. */
 		uint16_t max_wimm;
